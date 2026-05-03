ee97324a4160e7327e5873da85eb891fd456b52c
ramalyb <105539575+ramalyb@users.noreply.github.com>
2026-05-03 14:07:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in bahrain_vat/__init__.py
-from bahrain_vat import __version__ as version
-
 setup(
-	name='bahrain_vat',
-	version=version,
-	description='BAHRAIN VAT Management and Reporting',
-	author='ERPGulf',
-	author_email='support@erpgulf.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name="bahrain_vat",
+    version="0.0.1",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
 )
